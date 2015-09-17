6004df6b3cfb8beb57c0e116cb08e8a18bc06e88
Ryuichi UEDA <ryuichiueda@gmail.com>
2015-09-17 14:18:08 +0900

    Add a sample

---
+++ LED_SW_TOGGLE/led_sw_toggle.ino
@@ -0,0 +1,26 @@
+const int pin_led = 13;
+const int pin_sw = 7;
+
+void setup() {
+	pinMode(pin_sw, INPUT);
+	pinMode(pin_led, OUTPUT);
+}
+
+void toggleLed() {
+  static int onoff = 0;
+  onoff = 1 - onoff;
+  digitalWrite(pin_led, onoff);
+}
+
+void loop() {
+  static int led_val = 0;
+  static int push_prev = 0;
+
+  int push_now = digitalRead(pin_sw);
+
+  if(push_prev && !push_now)
+    toggleLed();
+
+  push_prev = push_now;
+  delay(50);
+}
