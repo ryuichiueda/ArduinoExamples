7fa1fbcad0899decc5e1c1ec79974561943aae42
Ryuichi UEDA <ryuichiueda@gmail.com>
2015-09-17 13:09:53 +0900

    Remove if

--- LED_SW_PUSH_ON/led_sw_push_on.ino
+++ LED_SW_PUSH_ON/led_sw_push_on.ino
@@ -2,13 +2,11 @@ const int pin_led = 13;
 const int pin_sw = 7;
 
 void setup() {
-	pinMode(7, INPUT);
-	pinMode(13, OUTPUT);
+	pinMode(pin_sw, INPUT);
+	pinMode(pin_led, OUTPUT);
 }
 
 void loop() {
-	if(digitalRead(pin_sw))
-		digitalWrite(pin_led,HIGH);
-	else
-		digitalWrite(pin_led,LOW);
+	int val = digitalRead(pin_sw);
+	digitalWrite(pin_led,val);
 }
